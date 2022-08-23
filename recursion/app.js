df94c1cd5c115e5eb1996bfd9530cd3eabf7970f
JASantia85 <JuanASantiago1058@gmail.com>
2022-08-23 11:01:40 -0400

    helper method recursion

--- recursion/app.js
+++ recursion/app.js
@@ -36,9 +36,33 @@
 
 // factorial(5)
 
-function factorial(num) {
-  if (num === 1) return 1;
-  return num * factorial(num - 1);
+// function factorial(num) {
+//   if (num === 1) return 1;
+//   return num * factorial(num - 1);
+// }
+
+// factorial(5)
+
+// Helper method recursion 
+function collectOddValues(arr){
+    
+  let result = [];
+
+  function helper(helperInput){
+      if(helperInput.length === 0) {
+          return;
+      }
+      
+      if(helperInput[0] % 2 !== 0){
+          result.push(helperInput[0])
+      }
+      
+      helper(helperInput.slice(1))
+  }
+  
+  helper(arr)
+
+  return result;
 }
 
-factorial(5)
+collectOddValues([1,2,3,4,5,6,7,8,9])

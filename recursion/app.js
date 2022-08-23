c782b834a9df8861b6bc86fbec7e8b14bf0c2101
JASantia85 <JuanASantiago1058@gmail.com>
2022-08-23 10:30:55 -0400

    manually run through recursive call

--- recursion/app.js
+++ recursion/app.js
@@ -1,11 +1,24 @@
-function countDown(num) {
-  if (num<= 0) {
-    console.log('All done!');
-    return;
-  }
-  console.log(num);
-  num--;
-  countDown(num)
+// function countDown(num) {
+//   if (num<= 0) {
+//     console.log('All done!');
+//     return;
+//   }
+//   console.log(num);
+//   num--;
+//   countDown(num)
+// }
+
+// countDown(10)
+
+// second case 
+function sumRange(num) {
+  if(num === 1) return 1;
+  return num + sumRange(num - 1);
 }
 
-countDown(10)
+sumRange(4)
+
+// return 4 + sumRange(3)
+// return 3 + sumRange(2)
+// return 2 + sumRange(1)
+// return 1

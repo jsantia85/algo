11888b29f6b322ad03bc98f22d878f4955d37364
JASantia85 <JuanASantiago1058@gmail.com>
2022-08-23 12:58:29 -0400

    pure recursion

--- recursion/app.js
+++ recursion/app.js
@@ -44,25 +44,43 @@
 // factorial(5)
 
 // Helper method recursion 
-function collectOddValues(arr){
+// function collectOddValues(arr){
     
-  let result = [];
+//   let result = [];
 
-  function helper(helperInput){
-      if(helperInput.length === 0) {
-          return;
-      }
+//   function helper(helperInput){
+//       if(helperInput.length === 0) {
+//           return;
+//       }
       
-      if(helperInput[0] % 2 !== 0){
-          result.push(helperInput[0])
-      }
+//       if(helperInput[0] % 2 !== 0){
+//           result.push(helperInput[0])
+//       }
       
-      helper(helperInput.slice(1))
-  }
+//       helper(helperInput.slice(1))
+//   }
   
-  helper(arr)
+//   helper(arr)
 
-  return result;
+//   return result;
+// }
+
+// collectOddValues([1,2,3,4,5,6,7,8,9])
+
+// Pure recursion
+function collectOddValues(arr){
+  let newArr = [];
+  
+  if(arr.length === 0) {
+      return newArr;
+  }
+      
+  if(arr[0] % 2 !== 0){
+      newArr.push(arr[0]);
+  }
+      
+  newArr = newArr.concat(collectOddValues(arr.slice(1)));
+  return newArr;
 }
 
-collectOddValues([1,2,3,4,5,6,7,8,9])
+collectOddValues([1,2,3,4,5])

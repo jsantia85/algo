d73aa57db2fe9731f54e4d8792344fc2b7c80820
JASantia85 <JuanASantiago1058@gmail.com>
2022-08-23 10:42:22 -0400

    write factorial recursively

--- recursion/app.js
+++ recursion/app.js
@@ -26,12 +26,19 @@
 // return 4 + 3 + 2 + 1 = 10
 
 // factorial 
-function factorial (num) {
-  let total = 1;
-  for (let i = num; i > 0; i--) {
-    total *= i
-  }
-  return total
+// function factorial (num) {
+//   let total = 1;
+//   for (let i = num; i > 0; i--) {
+//     total *= i
+//   }
+//   return total
+// }
+
+// factorial(5)
+
+function factorial(num) {
+  if (num === 1) return 1;
+  return num * factorial(num - 1);
 }
 
 factorial(5)

904ef951bd2517661d1cfb9a6ddfe7c4c7ce9acf
JASantia85 <JuanASantiago1058@gmail.com>
2022-08-23 10:36:22 -0400

    intro to factorial

--- recursion/app.js
+++ recursion/app.js
@@ -11,14 +11,27 @@
 // countDown(10)
 
 // second case 
-function sumRange(num) {
-  if(num === 1) return 1;
-  return num + sumRange(num - 1);
-}
+// function sumRange(num) {
+//   if(num === 1) return 1;
+//   return num + sumRange(num - 1);
+// }
 
-sumRange(4)
+// sumRange(4)
 
 // return 4 + sumRange(3)
 // return 3 + sumRange(2)
 // return 2 + sumRange(1)
 // return 1
+
+// return 4 + 3 + 2 + 1 = 10
+
+// factorial 
+function factorial (num) {
+  let total = 1;
+  for (let i = num; i > 0; i--) {
+    total *= i
+  }
+  return total
+}
+
+factorial(5)

2c2a378877b5e6252a577a5a5839dd4807429fd3
JASantia85 <JuanASantiago1058@gmail.com>
2022-08-30 10:09:33 -0400

    stub up function

--- recursion/app.js
+++ recursion/app.js
@@ -68,19 +68,23 @@
 // collectOddValues([1,2,3,4,5,6,7,8,9])
 
 // Pure recursion
-function collectOddValues(arr){
-  let newArr = [];
+// function collectOddValues(arr){
+//   let newArr = [];
   
-  if(arr.length === 0) {
-      return newArr;
-  }
+//   if(arr.length === 0) {
+//       return newArr;
+//   }
       
-  if(arr[0] % 2 !== 0){
-      newArr.push(arr[0]);
-  }
+//   if(arr[0] % 2 !== 0){
+//       newArr.push(arr[0]);
+//   }
       
-  newArr = newArr.concat(collectOddValues(arr.slice(1)));
-  return newArr;
-}
+//   newArr = newArr.concat(collectOddValues(arr.slice(1)));
+//   return newArr;
+// }
 
-collectOddValues([1,2,3,4,5])
+// Coding Exercise 10: power 
+
+function power() {
+  
+}

facb33f1df34dc14586941bfaba5132cff6b1519
JASantia85 <JuanASantiago1058@gmail.com>
2022-08-30 10:30:05 -0400

    create new folder, move initial problems, and stub up new problem

---
+++ recursion-problem-set/app.js
@@ -0,0 +1,19 @@
+// Coding Exercise 10: power 
+
+// function power(b, e) {
+//   if (e === 0){
+//     return 1;
+//   } else {
+//     return b * power(b, e - 1)
+//   }
+// }
+
+// console.log(power(5,5))
+
+// Coding Exercise 11: factorial
+
+function factorial() {
+
+
+  
+}
--- recursion/app.js
+++ recursion/app.js
@@ -82,15 +82,3 @@
 //   newArr = newArr.concat(collectOddValues(arr.slice(1)));
 //   return newArr;
 // }
-
-// Coding Exercise 10: power 
-
-// function power(b, e) {
-//   if (e === 0){
-//     return 1;
-//   } else {
-//     return b * power(b, e - 1)
-//   }
-// }
-
-// console.log(power(5,5))

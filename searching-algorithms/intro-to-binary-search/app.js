19b81873a42b36c69f8d32890ad0b770e05d3a08
JASantia85 <JuanASantiago1058@gmail.com>
2022-09-06 13:38:46 -0400

    stub up problem

---
+++ searching-algorithms/intro-to-binary-search/app.js
@@ -0,0 +1,13 @@
+// Binary Search Exercise
+// write a function that accepts a sorted array and a value
+//create a left pointer at the start of the array, and a right pointer at the end of the array
+// while the left pointer comes before the right pointer 
+  // create a pointer in the middle
+  // if you find the value you want, return the index
+  // if the value is too small, move the left pointer up
+  // if the value is too large, move the right pointer down
+// if you never find the value, return -1
+
+function binarySearch (arr, val) {
+
+}

97c5482110c10d95e1d95dbcbac7a7ce9467a7d5
JASantia85 <JuanASantiago1058@gmail.com>
2022-09-06 14:10:34 -0400

    refactor code

--- searching-algorithms/intro-to-binary-search/app.js
+++ searching-algorithms/intro-to-binary-search/app.js
@@ -11,20 +11,19 @@
 function binarySearch (arr, val) {
   let start = 0;
   let end = arr.length - 1;
-  let middle;
-
-  while (start <= end) {
-    middle = Math.floor((end + start) / 2);
-    if (arr[middle] === val) {
-      return middle
-    } else if (arr[middle] < val) {
-      start = middle + 1;
-    } else {
-      start = middle - 1;
+  let middle = Math.floor((end + start) / 2);
+    while (arr[middle] !== val && start <= end) {
+      if (val < arr[middle]) {
+        end = middle - 1
+      } else {
+        start =  middle + 1
+      }
+      middle = Math.floor((end + start) / 2);
     }
-  }
-
-  return -1 
+  if (arr[middle] === val) {
+    return middle
+  } 
+  return -1
 }
 
-console.log(binarySearch([1, 2, 3, 4, 5], 10))
+console.log(binarySearch([1, 2, 3, 4, 5, 10, 12, 25, 33, 45], 10))

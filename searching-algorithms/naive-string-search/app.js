127237eb96aa553efe385e46a8010abe01d46b3f
JASantia85 <JuanASantiago1058@gmail.com>
2022-09-06 15:35:26 -0400

    walkthrough example given

---
+++ searching-algorithms/naive-string-search/app.js
@@ -0,0 +1,25 @@
+// Naive String Search Implementation 
+// Loop over the longer string 
+// loop over the shorter string 
+// if the charachters don't match, break out of the inner loop
+// if the characters do match, keep going
+// if you complete the inner loop and find a match, increment the count of matches 
+// return the count
+
+function naiveSearch (long, short) {
+  let count = 0;
+
+  for (let i = 0; i < long.length; i++) {
+    for (let j = 0; j < short.length; j++) {
+      if (short[j] !== long[i+j]) {
+        break;
+      }
+      if (j === short.length - 1) {
+        count++;
+      }
+    }
+  }
+  return count
+}
+
+console.log(naiveSearch('Why is this here', 'is'))
